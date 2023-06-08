369effcc15f59ead6a9590fcdf57830d868f49a1
Ananda Satria <anandasatriaf6@gmail.com>
2023-06-08 22:53:35 +0700

    Membuat Responsive

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,34 @@
 /* ========================  FITUR MODAL POPUP  ======================== */
 
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
 span.onclick = function() {
   modal.style.display = "none";
+  modal2.style.display = "none";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
 
